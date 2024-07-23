0573d17ec08897c39d405eb01751428255153163
popoolahussein <popoolakehindehussein@gmail.com>
2024-07-23 02:02:09 +0100

    docs: implemented UUID import in book.js

--- src/js/book.js
+++ src/js/book.js
@@ -1,7 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class Book {
-  constructor(title, author) {
+  constructor(title, author, id = uuidv4()) {
     this.title = title;
     this.author = author;
+    this.id = id;
   }
 
   getTitle() {

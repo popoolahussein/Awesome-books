3ea45e5acde5129ad1230d24abb3e565fcbc23de
popoolahussein <popoolakehindehussein@gmail.com>
2024-07-28 23:35:40 +0100

    docs: added functionalities to activate the removed books menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 import Library from './library.js';
 
-document.addEventListener('DOMContentLoaded', Library.init);
+document.addEventListener('DOMContentLoaded', () => {
+  Library.init();
+
+  const books = Library.getBooksFromLocalStorage();
+  const removedBooks = Library.getRemovedBooksFromLocalStorage();
+  Library.displayBooks(books, removedBooks);
+
+  const hamburgerButton = document.getElementById('bin-button');
+  const mobileMenu = document.getElementById('menu');
+
+  hamburgerButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('visible');
+  });
+
+  mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('visible');
+  });
+});

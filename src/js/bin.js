8fc299a9df18c0dcf15e10cf2e85703e059b2470
popoolahussein <popoolakehindehussein@gmail.com>
2024-07-28 22:51:58 +0100

    feat: removed file

--- src/js/bin.js
+++
@@ -1,7 +0,0 @@
-import Library from './library.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const books = Library.getBooksFromLocalStorage();
-  const removedBooks = Library.getRemovedBooksFromLocalStorage();
-  Library.displayBooks(books, removedBooks);
-});
